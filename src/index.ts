1910331106bb63f98f2de5ffae80fd3f40f687f1
RenoOx <122762241+RenoOx@users.noreply.github.com>
2026-05-16 18:52:03 -0500

    fix apuntando al frontend del cors

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const app = new Hono();
 app.use(
   "*",
   cors({
-    origin: "https://proyecto-roma-production.up.railway.app",
+    origin: "https://roma-frontend-chi.vercel.app/",
   }),
 );
 
